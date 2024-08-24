3f3197f18c7914fc12e51c059eb931dfc2f69e6c
Jacob Kerr <jck268@cornell.edu>
2024-08-24 00:26:24 -0400

    Fix script

--- index.js
+++ index.js
@@ -1,22 +1,24 @@
-const names = require("all-the-package-names")
-var fs = require('fs');
-const util = require('util');
-const exec = util.promisify(require('child_process').exec);
+import names from "all-the-package-names" with { type: "json" };
+import { readFileSync, writeFileSync } from 'fs';
+import { promisify } from 'util';
+import childProcess from 'child_process';
+const exec = promisify(childProcess.exec);
 
 
-let manifest = JSON.parse(fs.readFileSync('package.json', 'utf8'));
+let manifest = JSON.parse(readFileSync('package.json', 'utf8'));
 
 console.log('start');
-names.forEach(async name => {
+for(let i = 0; i < names.length; i++) {
+    let name = names[i];
     let version = '*';
     console.log(`start ${name}: ${version}`);
     manifest.dependencies[name] = version;
-    fs.writeFileSync('package.json', JSON.stringify(manifest));
+    writeFileSync('package.json', JSON.stringify(manifest));
     try {
         await exec('yarn install');
     } catch (err) {
         delete manifest.dependencies[name];
     }
     console.log(`finish ${name}: ${version}`);
-});
+}
 console.log('end');
